89ab01398de9c6bc2c9c80e777861cb448ccbfb2
AleksanderSkubala <alexskubala@gmail.com>
2021-02-26 22:06:16 +0100

    initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,25 @@
+<script>
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { VuiSample } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  // components: {
+  //  VuiSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vui-sample />
+    <vui-input
+      v-model="firstName"
+      name="firstName"
+      label="First Name"
+      placeholder="e.g. John"
+      autocomplete="fname"
+    />
+  </div>
+</template>
---
+++ src/lib-components/vui-input.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="vui-input">
+    <label :for="name">{{ label }}</label>
+    <div :class="['inputWrapper', { 'inputWrapper--outline': outline }]">
+      <input
+        type="text"
+        :name="name"
+        :placeholder="placeholder"
+        @input="$emit('input', $event.target.value)"
+        :autocomplete="autocomplete"
+      />
+      <div class="line"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'VuiInput',
+  props: {
+    placeholder: {
+      required: true,
+      type: String,
+    },
+    name: {
+      required: true,
+      type: String,
+    },
+    label: {
+      required: true,
+      type: String,
+    },
+    autocomplete: {
+      required: false,
+      type: String,
+    },
+    outline: {
+      required: false,
+      type: Boolean,
+    },
+  },
+})
+</script>
+
+<style lang="scss">
+  .vui-input {
+    color: gray;
+  }
+
+  .inputWrapper {
+    margin-top: 10px;
+    position: relative;
+
+    input {
+      padding: 10px 12px;
+      border: none;
+      border-radius: 4px;
+      background: #ececec;
+      outline: none;
+      caret-color: #2196f3;
+    }
+
+    .line {
+      border-radius: 0 0 3px 3px;
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background: #2196f3;
+      height: 2px;
+      opacity: 0;
+      transform: scaleX(0.4);
+      transition: transform 0.2s ease;
+    }
+
+    input:focus ~ .line {
+      opacity: 1;
+      transform: scaleX(1);
+    }
+  }
+
+  .inputWrapper--outline {
+    input {
+      box-shadow: none;
+      transition: box-shadow 0.2s ease;
+    }
+
+    input:focus {
+      box-shadow: 0 0 0 4px rgba(33, 150, 243, 0.5);
+    }
+
+    .line {
+      display: none;
+    }
+  }
+</style>
